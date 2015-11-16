0db2f9213535980c7747a9328c4e8fe2eceafdaf
BW <bw@localhost.localdomain>
2015-11-16 21:43:45 +0900

    Graph

--- Visual Studio 2013/Projects/Algorithm/GreedyApproach/Huffman.h
+++ Visual Studio 2013/Projects/Algorithm/GreedyApproach/Huffman.h
@@ -15,34 +15,42 @@ typedef unsigned int UINT;
 typedef unsigned char UCHAR;
 
 typedef struct HuffmanSymbolInfo {
-	UCHAR symbol;
-	int frequency;
+    UCHAR symbol;
+    int frequency;
 } SymbolInfo;
 
 typedef struct HuffmanNode {
-	SymbolInfo data;
-	struct HuffmanNode* left;
-	struct HuffmanNode* right;
+    SymbolInfo data;
+    struct HuffmanNode *left;
+    struct HuffmanNode *right;
 } Node;
 
 typedef struct BitBuffer {
-	UCHAR* buffer;
-	UINT size;
+    UCHAR *buffer;
+    UINT size;
 } BitBuffer;
 
 typedef struct HuffmanCode {
-	UCHAR code[MAX_CHAR];
-	int size;
+    UCHAR code[MAX_CHAR];
+    int size;
 } HuffmanCode;
 
-void HuffmanEncode(Node** node, UCHAR* s, BitBuffer* encode, HuffmanCode codeTable[MAX_CHAR]);
-void HuffmanDecode(Node* tree, BitBuffer* endoded, UCHAR* decoded);
-void buildPrefixTree(Node** tree, SymbolInfo symbolTable[MAX_CHAR]);
-void buildCodeTable(Node* tree, HuffmanCode codeTable[MAX_CHAR], UCHAR code[MAX_BIT], int size);
-void addBit(BitBuffer* buffer, char value);
-void printBinary(BitBuffer* buffer);
-void HuffmanDestroyTree(Node* Node);
-void HuffmanDestroyNode(Node* Node);
-Node* createNode(SymbolInfo newData);
+void HuffmanEncode(Node **node, UCHAR *s, BitBuffer *encode, HuffmanCode codeTable[MAX_CHAR]);
+
+void HuffmanDecode(Node *tree, BitBuffer *endoded, UCHAR *decoded);
+
+void buildPrefixTree(Node **tree, SymbolInfo symbolTable[MAX_CHAR]);
+
+void buildCodeTable(Node *tree, HuffmanCode codeTable[MAX_CHAR], UCHAR code[MAX_BIT], int size);
+
+void addBit(BitBuffer *buffer, char value);
+
+void printBinary(BitBuffer *buffer);
+
+void HuffmanDestroyTree(Node *node);
+
+void HuffmanDestroyNode(Node *node);
+
+Node *createNode(SymbolInfo newData);
 
 #endif
--- Visual Studio 2013/Projects/Algorithm/Hash/Chained_Hash.cpp
+++ Visual Studio 2013/Projects/Algorithm/Hash/Chained_Hash.cpp
@@ -1,126 +1,120 @@
 #include <iostream>
 #include <memory>
 #include <string>
+
 using namespace std;
 
-class Node
-{
+class Node {
 private:
-	int key;
-	int data;
-	Node *NextNode;
+    int key;
+    int data;
+    Node *NextNode;
 public:
-	friend class Table;
+    friend class Table;
 
-	Node(int k = 0, int d = 0);
+    Node(int k = 0, int d = 0);
 
-	void SetKey(int k);
-	void SetData(int d);
+    void SetKey(int k);
+
+    void SetData(int d);
 };
-Node::Node(int k, int d) : key(k), data(d)	{}
-void Node::SetKey(int k)	{ key = k; }
-void Node::SetData(int d)	{ data = d; }
-class Table
-{
+
+Node::Node(int k, int d) : key(k), data(d) { }
+
+void Node::SetKey(int k) { key = k; }
+
+void Node::SetData(int d) { data = d; }
+
+class Table {
 private:
-	Node **table;
-	int capacity;
+    Node **table;
+    int capacity;
 public:
-	friend class Node;
+    friend class Node;
 
-	Table(int capacity = 5);
+    Table(int capacity = 5);
 
-	Node** GetTable();
-	void SetTable(int k, int d);
+    Node **GetTable();
 
-	int HashFunc(int key);
+    void SetTable(int k, int d);
 
-	int Search(int k);
+    int HashFunc(int key);
 
-	void PrintTable();
+    int Search(int k);
+
+    void PrintTable();
 };
-Table::Table(int capacity)
-{
-	this->capacity = capacity;
-	table = new Node*[capacity];
-	memset(table, NULL, sizeof(int*) * capacity);
+
+Table::Table(int capacity) {
+    this->capacity = capacity;
+    table = new Node *[capacity];
+    memset(table, NULL, sizeof(int *) * capacity);
 }
 
-Node** Table::GetTable()	{ return table; }
-void Table::SetTable(int k, int d)
-{
-	Node *n = new Node(k, d);
+Node **Table::GetTable() { return table; }
 
-	if (table[HashFunc(n->key)] == NULL)
-		table[HashFunc(n->key)] = n;
-	else
-	{
-		cout << "�浹" << endl;
-		n->NextNode = table[HashFunc(n->key)];
-		table[HashFunc(n->key)] = n;
-	}
+void Table::SetTable(int k, int d) {
+    Node *n = new Node(k, d);
+
+    if (table[HashFunc(n->key)] == NULL)
+        table[HashFunc(n->key)] = n;
+    else {
+        cout << "충돌" << endl;
+        n->NextNode = table[HashFunc(n->key)];
+        table[HashFunc(n->key)] = n;
+    }
 }
 
-int Table::HashFunc(int key)	{ return key % capacity; }
+int Table::HashFunc(int key) { return key % capacity; }
 
-int Table::Search(int k)
-{
-	Node *cur = new Node;
+int Table::Search(int k) {
+    Node *cur = new Node;
 
-	for (int i = 0; i < capacity; i++)
-	{
-		cur = table[i];
-		while (cur != NULL)
-		{
-			if (cur->key == k)
-				return cur->data;
-			else
-				cur = cur->NextNode;
-		}
-	}
+    for (int i = 0; i < capacity; i++) {
+        cur = table[i];
+        while (cur != NULL) {
+            if (cur->key == k)
+                return cur->data;
+            else
+                cur = cur->NextNode;
+        }
+    }
 }
 
-void Table::PrintTable()
-{
-	Node *cur = new Node;
+void Table::PrintTable() {
+    Node *cur = new Node;
 
-	for (int i = 0; i < capacity; i++)
-	{
-		cur = table[i];
-		cout << i << " ";
-		while (cur != NULL)
-		{
-			cout << "k : " << cur->key << " " << "d : " << cur->data << " ";
-			cur = cur->NextNode;
-		}
-		cout << endl;
-	}
+    for (int i = 0; i < capacity; i++) {
+        cur = table[i];
+        cout << i << " ";
+        while (cur != NULL) {
+            cout << "k : " << cur->key << " " << "d : " << cur->data << " ";
+            cur = cur->NextNode;
+        }
+        cout << endl;
+    }
 }
 
-int main()
-{
-	Table t;
-	string command;
+int main() {
+    Table t;
+    string command;
 
-	while (true)
-	{
-		cin >> command;
+    while (true) {
+        cin >> command;
 
-		if (command.compare("node") == 0)
-		{
-			int k, d;
-			cin >> k >> d;
-			t.SetTable(k, d);
-		}
-		else if (command.compare("search") == 0)
-		{
-			int k;
-			cin >> k;
-			cout << "Data : " << t.Search(k) << endl;
-		}
-		else if (command.compare("print") == 0)
-			t.PrintTable();
-		else if (command.compare("quit") == 0)
-			break;
-	}
+        if (command.compare("node") == 0) {
+            int k, d;
+            cin >> k >> d;
+            t.SetTable(k, d);
+        }
+        else if (command.compare("search") == 0) {
+            int k;
+            cin >> k;
+            cout << "Data : " << t.Search(k) << endl;
+        }
+        else if (command.compare("print") == 0)
+            t.PrintTable();
+        else if (command.compare("quit") == 0)
+            break;
+    }
 }
